f6ee3dda5902dee47292658c62726fce54e3a045
Deathgun122 <brohan275@gmail.com>
2025-05-05 18:18:07 +0530

    Components Made

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+import Main from "@/components/Main";
+
+export const metadata = {
+    title: "Broodl ⋅ Dashboard"
+  };
+
+export default function DashboardPage() {
+    return (
+      <Main>
+        <h1>Dashboard</h1>
+      </Main>
+    );
+}
---
+++ components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function (props) {
+  const { children } = props
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
