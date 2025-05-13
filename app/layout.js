76d22810743bcaec160068680a09dd420e49c629
Rohan <brohan275@gmail.com>
2025-05-13 13:45:29 +0530

    Project Done

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { AuthProvider } from "@/context/AuthContext";
+import Head from "./head";
+import Logout from "@/components/Logout";
+
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
@@ -13,10 +18,10 @@ export default function RootLayout({ children }) {
 
   const header = (
     <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
-      <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Broodl</h1>
-      <div className="flex items-center justify-between">
-        Placeholder
-      </div>
+      <Link href={'/'}>
+        <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Broodl</h1>
+      </Link>
+      <Logout/>
     </header>
   );
 
@@ -27,11 +32,14 @@ export default function RootLayout({ children }) {
   )
   return (
     <html lang="en">
+      <Head/>
+      <AuthProvider>
       <body className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800  ' + opensans.className}>
         {header}
         {children}
         {footer}
       </body>
+      </AuthProvider>
     </html>
   );
 }
